945fcb6aa4309aad7303cf3195a8829274a38b81
Leo Wong <gnowoel@gmail.com>
2010-01-24 06:06:29 +0800

    putting sessions in the database

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100120011743) do
+ActiveRecord::Schema.define(:version => 20100123220532) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -17,5 +17,15 @@ ActiveRecord::Schema.define(:version => 20100120011743) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
