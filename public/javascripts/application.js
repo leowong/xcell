aa61e9014ccc777445be4fd7cefbd41d94218d4c
Leo Wong <gnowoel@gmail.com>
2010-02-20 10:14:04 +0800

    auto fading out flash messages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+document.observe("dom:loaded", function() {
+  setTimeout(hideFlashMessages, 5000);
+});
+
+function hideFlashMessages() {
+  $$('div#flash_message, div#flash_notice, div#flash_warning, div#flash_error').each(function(e) { 
+    if (e) Effect.Fade(e, { duration: 1.0 });
+  });
+}
